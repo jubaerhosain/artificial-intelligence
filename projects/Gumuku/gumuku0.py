dff32b427129a1718c5ce8cbdc5a363170f9728b
JubaerHosain <jubaerhosain1119@gmail.com>
2023-08-31 04:59:13 +0600

    updated

--- projects/Gumuku/gumuku0.py
+++ projects/Gumuku/gumuku0.py
@@ -3,6 +3,14 @@ class Point:
         self.x = x
         self.y = y
 
+    def __eq__(self, other):
+        if isinstance(other, Point):
+            return self.x == other.x and self.y == other.y
+        return False
+
+    def __hash__(self):
+        return hash((self.x, self.y))
+
 
 class Gumuku:
 
@@ -19,14 +27,31 @@ class Gumuku:
         self.EMPTY_MARKER = 0
 
         self.current_plyer = self.PLAYER_MARKER
+        
+        self.iteration = 0
 
-    def __get_available_moves(self) -> [Point]:
+    def __is_valid_point(self, point):
+        return point.x >= 0 and point.x < self.board_size and point.y >= 0 and point.y < self.board_size
+
+    def __get_available_moves(self):
         available_moves = []
         for i in range(self.board_size):
             for j in range(self.board_size):
                 if (self.board[i][j] == self.EMPTY_MARKER):
                     available_moves.append(Point(i, j))
         return available_moves
+
+    def __get_available_moves_1(self):
+        uniq_points = set()
+        dir = [[0, 1], [1, 0], [0, -1], [-1, 0], [-1, 1], [1, -1], [1, 1], [-1, -1]]
+        for i in range(self.board_size):
+            for j in range(self.board_size):
+                if(self.board[i][j] != self.EMPTY_MARKER):
+                    for p in dir:
+                        x, y = p[0] + i, p[1] + j
+                        if self.__is_valid_point(Point(x, y)) and self.board[x][y] == self.EMPTY_MARKER:
+                            uniq_points.add(Point(x, y))
+        return uniq_points
 
     def __is_board_full(self) -> bool:
         return all(self.board[i][j] != self.EMPTY_MARKER for i in range(self.board_size) for j in range(self.board_size))
@@ -81,6 +106,8 @@ class Gumuku:
     def __minimax_alpha_beta_pruning(self, marker, depth, alpha, beta):
         best_move = Point(-1, -1)
         best_score = -self.inf if marker == self.AI_MARKER else self.inf
+        
+        self.iteration += 1
 
         if self.is_won(self.AI_MARKER):
             return best_move, self.inf
@@ -89,11 +116,12 @@ class Gumuku:
         elif self.__is_board_full():
             return best_move, 0
         
-        if (depth >= 4):
+        if (depth >= 5):
             # print(depth)
             return best_move, 0
 
-        for current_move in self.__get_available_moves():
+
+        for current_move in self.__get_available_moves_1():
             self.board[current_move.x][current_move.y] = marker
             if marker == self.AI_MARKER:
                 _, current_score = self.__minimax_alpha_beta_pruning(
@@ -120,14 +148,26 @@ class Gumuku:
         return best_move, best_score
 
     def get_ai_move(self) -> Point:
+        self.iteration = 0
         best_move, _ = self.__minimax_alpha_beta_pruning(
             self.AI_MARKER, 0, -self.inf, self.inf)
+        if best_move.x == -1 and best_move.y == -1:
+            for point in self.__get_available_moves_1():
+                return point
+            
+        print(self.iteration)
         return best_move
 
 
 if __name__ == "__main__":
     gumuku = Gumuku(10, 5)
     current_player = gumuku.PLAYER_MARKER
+    
+    # gumuku.board[5][5] = 1
+    
+    # pts = gumuku._Gumuku__get_available_moves_1()
+    # for pt in pts:
+    #     print(pt.x, pt.y)
 
     gumuku.print_board()
 
